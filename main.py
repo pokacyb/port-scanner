3e01f985268ffb46d639f07e08cab5c5b37d9342
pokacyb <pokacyb@tutanota.com>
2021-12-17 19:47:27 +0100

    Use user input for port range

--- main.py
+++ main.py
@@ -4,25 +4,34 @@ from unittest import main
 print("What is the url/IP you would like to scan?")
 URLorIP = input()
 
-# Called with URL
-ports = port_scanner.get_open_ports(URLorIP, [75,85])
+print("Enter the port as an integer:")
+targettedPort = int(input())
+print("Enter the port as an integer:")
+targettedPort2 = int(input())
+
+
+ports = port_scanner.get_open_ports(URLorIP, [targettedPort,targettedPort2])
 print("Open ports:", ports)
 
-# Called with ip address
-ports = port_scanner.get_open_ports("104.26.10.78", [8079, 8090])
-print("Open ports:", ports)
+# # Called with URL   
+# ports = port_scanner.get_open_ports(URLorIP, [75,85])
+# print("Open ports:", ports)
 
-# Verbose called with ip address and no host name returned -- single open port
-ports = port_scanner.get_open_ports("104.26.10.78", [440, 450], True)
-print(ports + '\n')
+# # Called with ip address
+# ports = port_scanner.get_open_ports("104.26.10.78", [8079, 8090])
+# print("Open ports:", ports)
 
-# Verbose called with ip address and valid host name returned -- single open port
-ports = port_scanner.get_open_ports("137.74.187.104", [440, 450], True)
-print(ports + '\n')
+# # Verbose called with ip address and no host name returned -- single open port
+# ports = port_scanner.get_open_ports("104.26.10.78", [440, 450], True)
+# print(ports + '\n')
 
-# Verbose called with host name -- multiple ports returned
-ports = port_scanner.get_open_ports("scanme.nmap.org", [20, 80], True)
-print(ports + '\n')
+# # Verbose called with ip address and valid host name returned -- single open port
+# ports = port_scanner.get_open_ports("137.74.187.104", [440, 450], True)
+# print(ports + '\n')
 
-# Run unit tests automatically
-# main(module='test_module', exit=False)
+# # Verbose called with host name -- multiple ports returned
+# ports = port_scanner.get_open_ports("scanme.nmap.org", [20, 80], True)
+# print(ports + '\n')
+
+# # Run unit tests automatically
+# # main(module='test_module', exit=False)
