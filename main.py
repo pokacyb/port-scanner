6527b2d95053655bb1ea3453ca796a44dceba9aa
pokacyb <pokacyb@tutanota.com>
2021-12-17 20:03:26 +0100

    Remove fluff

--- main.py
+++ main.py
@@ -16,25 +16,5 @@ except ValueError:
 ports = port_scanner.get_open_ports(URLorIP, [startRange,endRange])
 print("Open ports:", ports)
 
-# # Called with URL   
-# ports = port_scanner.get_open_ports(URLorIP, [75,85])
-# print("Open ports:", ports)
-
-# # Called with ip address
-# ports = port_scanner.get_open_ports("104.26.10.78", [8079, 8090])
-# print("Open ports:", ports)
-
-# # Verbose called with ip address and no host name returned -- single open port
-# ports = port_scanner.get_open_ports("104.26.10.78", [440, 450], True)
-# print(ports + '\n')
-
-# # Verbose called with ip address and valid host name returned -- single open port
-# ports = port_scanner.get_open_ports("137.74.187.104", [440, 450], True)
-# print(ports + '\n')
-
-# # Verbose called with host name -- multiple ports returned
-# ports = port_scanner.get_open_ports("scanme.nmap.org", [20, 80], True)
-# print(ports + '\n')
-
-# # Run unit tests automatically
-# # main(module='test_module', exit=False)
+# Run unit tests automatically
+# main(module='test_module', exit=False)
